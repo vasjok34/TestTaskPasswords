76da8b9919f9800e27670b692658340560629878
Vasyl Hlyva <glivavasa@gmail.com>
2023-04-19 12:47:39 +0300

    Initial commit

---
+++ Extensions/PasswordExtensions.cs
@@ -0,0 +1,13 @@
+﻿using TestTaskPasswords.Models;
+
+namespace TestTaskPasswords.Extensions;
+
+public static class PasswordExtensions
+{
+    public static bool IsPasswordValid(this PasswordModel passwordModel)
+    {
+        int occurrences = passwordModel.Password.Count(x => x == passwordModel.RequiredSymbol);
+
+        return occurrences >= passwordModel.MinOccurrences && occurrences <= passwordModel.MaxOccurrences;
+    }
+}
---
+++ Models/PasswordModel.cs
@@ -0,0 +1,17 @@
+﻿namespace TestTaskPasswords.Models;
+
+public class PasswordModel
+{
+    public PasswordModel(string password, char requiredSymbol, int minOccurrences, int maxOccurrences)
+    {
+        Password = password;
+        RequiredSymbol = requiredSymbol;
+        MinOccurrences = minOccurrences;
+        MaxOccurrences = maxOccurrences;
+    }
+
+    public string Password { get; set; }
+    public char RequiredSymbol { get; set; }
+    public int MinOccurrences { get; set; }
+    public int MaxOccurrences { get; set; }
+}
